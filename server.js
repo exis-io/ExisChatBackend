3e9d12fa052b5c51039ec26884c49eeb886e5ead
Nicholas Hyatt <nhyatt@wisc.edu>
2016-03-15 12:58:09 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var riffle = require('jsriffle');
 
 var server = riffle.Domain(process.env.Domain);
-var app = server.linkDomain('xs.demo.nickh.chat');
+var app = server.linkDomain('xs.demo.nick2.chat');
 
 server.onJoin = function() {
   app.subscribe('exisChat', function(msg){
