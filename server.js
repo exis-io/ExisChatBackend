ed3f3de38c18edafa40821c6bfd31a42046f9f57
Nicholas Hyatt <nhyatt@wisc.edu>
2016-03-15 13:01:26 -0500

    fixed server.js errors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var riffle = require('jsriffle');
 
-var server = riffle.Domain(process.env.Domain);
+var server = riffle.Domain(process.env.DOMAIN);
 var app = server.linkDomain('xs.demo.nick2.chat');
 
 server.onJoin = function() {
