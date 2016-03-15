49d941404c7da5ae0ad481020d3c5dc17a8258c3
Nicholas Hyatt <nhyatt@wisc.edu>
2016-03-15 13:42:42 -0500

    appdomain

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var riffle = require('jsriffle');
 
 var server = riffle.Domain(process.env.DOMAIN);
-var app = server.linkDomain('xs.demo.nick2.chat');
+
+//this will get the top level app domain this will be in process.env.APPDOMAIN coming soon
+var appdomain = process.env.DOMAIN.split('.').slice(0,-2).join('.');
+
+var app = server.linkDomain(appdomain);
 
 server.onJoin = function() {
   app.subscribe('exisChat', function(msg){
